46e94f453d6a1eb3971c22516efc13a3adfe19bc
Josh Reno <jreno3@gatech.edu>
2017-11-11 22:29:04 -0500

    added enum and cases

--- main.c
+++ main.c
@@ -1,7 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "myLib.h"
 
-int main() {
-	
+enum GBAState {
+	START,
+	LOKI
+	HULK
+	HELA
+	LOSE
+	GAMEOVER
+}
+
+int main(void) {
+	REG_DISPCNT = MODE_3 | BG2_ENABLE;
+
+	enum GBAState state = START;
+
+	while (1)
+	{
+		waitForVBlank();
+		switch(state)
+		{
+			case START:
+				break;
+
+			case LOKI:
+				break;
+
+			case HULK:
+				break;
+
+			case HELA:
+				break;
+
+			case LOSE:
+				break;
+
+			case GAMEOVER:
+				break;
+		}
+	}
+
+	return 0;
 }
