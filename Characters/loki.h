eea98aacae8ed25512ed37882875924dbd5034b2
Josh Reno <jreno3@gatech.edu>
2017-11-11 21:39:33 -0500

    added font and myLib

---
+++ Characters/loki.h
@@ -0,0 +1,6 @@
+#ifndef LOKI_BITMAP_H
+#define LOKI_BITMAP_H
+extern const unsigned short loki[2400];
+#define LOKI_WIDTH 40
+#define LOKI_HEIGHT 60
+#endif
---
+++ Characters/thor.h
@@ -0,0 +1,6 @@
+#ifndef THOR_BITMAP_H
+#define THOR_BITMAP_H
+extern const unsigned short thor[2400];
+#define THOR_WIDTH 40
+#define THOR_HEIGHT 60
+#endif
