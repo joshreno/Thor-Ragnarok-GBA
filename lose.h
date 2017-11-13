8f21e7cbd3af1fae5061dbad2bddd4d84dc8c76c
jreno3 <joshuagregr@gmail.com>
2017-11-13 01:44:41 -0500

    changed lose file

--- lose.h
+++ lose.h
@@ -1,23 +1,6 @@
-/*
- * Exported with nin10kit v1.7
- * Invocation command was nin10kit --mode=3 lose 675527eaeef5851.png 
- * Time-stamp: Saturday 11/11/2017, 22:46:25
- * 
- * Image Information
- * -----------------
- * 675527eaeef5851.png 930@700
- * 
- * All bug reports / feature requests are to be filed here https://github.com/TricksterGuy/nin10kit/issues
- */
-
-#ifndef LOSE_H
-#define LOSE_H
-
-extern const unsigned short lose[651000];
-#define lose_SIZE 1302000
-#define lose_LENGTH 651000
-#define lose_WIDTH 930
-#define lose_HEIGHT 700
-
+#ifndef LOSE_BITMAP_H
+#define LOSE_BITMAP_H
+extern const unsigned short lose[38400];
+#define LOSE_WIDTH 240
+#define LOSE_HEIGHT 160
 #endif
-
