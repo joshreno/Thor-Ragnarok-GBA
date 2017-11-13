bb3324c91f3717fbdd62812163f06c59c1141fe4
jreno3 <joshuagregr@gmail.com>
2017-11-13 17:14:39 -0500

    added images again

---
+++ characters.h
@@ -0,0 +1,6 @@
+#ifndef CHARACTERS_BITMAP_H
+#define CHARACTERS_BITMAP_H
+extern const unsigned short characters[38400];
+#define CHARACTERS_WIDTH 240
+#define CHARACTERS_HEIGHT 160
+#endif
---
+++ poster.h
@@ -0,0 +1,6 @@
+#ifndef POSTER_BITMAP_H
+#define POSTER_BITMAP_H
+extern const unsigned short poster[38400];
+#define POSTER_WIDTH 240
+#define POSTER_HEIGHT 160
+#endif
